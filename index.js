c4b50441c6e71c91faecd1ad585acef86e755209
ilijazivkovic94 <>
2022-11-08 13:01:48 -0500

    updated price getting logic

--- index.js
+++ index.js
@@ -41,46 +41,37 @@ app.get("/scrape", function (req, res) {
             product.name = $("title").text().trim().replace(/\t/g, '').replace(/\s\s/g, '').split('–')[0];
             product.description = $('meta[name="description"]').attr("content") || '';
             let images = await page.evaluate(() => {
-              let imageTags = document.getElementsByTagName("img");
+              let imageTags = document.getElementsByTagName('img');
               let limitHeight = 100;
               let limitWidth = 100;
               let removeQuery = false;
               let httpOnly = false;
               let useSrcset = false;
-              if (
-                window.location.href.indexOf("chewy.com") > -1 ||
-                window.location.href.indexOf("cvs.com") > -1
-              ) {
+              if (window.location.href.indexOf('chewy.com') > -1 || window.location.href.indexOf('cvs.com') > -1) {
                 imageTags = document.querySelectorAll("main img");
               }
-              if (window.location.href.indexOf("costco.com") > -1) {
+              if (window.location.href.indexOf('costco.com') > -1) {
                 imageTags = document.querySelectorAll("#product-page img");
               }
-              if (window.location.href.indexOf("samsclub.com") > -1) {
-                imageTags = document.querySelectorAll(
-                  ".sc-pc-large-desktop-layout-columns img"
-                );
+              if (window.location.href.indexOf('samsclub.com') > -1) {
+                imageTags = document.querySelectorAll(".sc-pc-large-desktop-layout-columns img");
               }
-              if (window.location.href.indexOf("suitsupply.com") > -1) {
+              if (window.location.href.indexOf('suitsupply.com') > -1) {
                 imageTags = document.querySelectorAll(".pdp-images img");
               }
-              if (window.location.href.indexOf("therealreal.com") > -1) {
-                imageTags = document.querySelectorAll(
-                  ".pdp-desktop-images img"
-                );
+              if (window.location.href.indexOf('therealreal.com') > -1) {
+                imageTags = document.querySelectorAll(".pdp-desktop-images img");
                 httpOnly = true;
               }
-              if (window.location.href.indexOf("westelm.com") > -1) {
-                imageTags = document.querySelectorAll(
-                  "#pip-hero-container-WE img"
-                );
+              if (window.location.href.indexOf('westelm.com') > -1) {
+                imageTags = document.querySelectorAll("#pip-hero-container-WE img");
                 httpOnly = true;
               }
-              if (window.location.href.indexOf("hillhousehome.com") > -1) {
+              if (window.location.href.indexOf('hillhousehome.com') > -1) {
                 imageTags = document.querySelectorAll(".pdpCarousel img");
                 useSrcset = true;
               }
-              if (window.location.href.indexOf("jcrew.com") > -1) {
+              if (window.location.href.indexOf('jcrew.com') > -1) {
                 imageTags = document.querySelectorAll("#c-product__photos img");
                 limitHeight = 70;
                 limitWidth = 70;
@@ -88,57 +79,42 @@ app.get("/scrape", function (req, res) {
                 httpOnly = true;
               }
 
-              if (window.location.href.indexOf("shop.lululemon.com") > -1) {
-                imageTags = document.querySelectorAll(
-                  ".pdp-carousel-images-offset img"
-                );
+              if (window.location.href.indexOf('shop.lululemon.com') > -1) {
+                imageTags = document.querySelectorAll(".pdp-carousel-images-offset img");
                 limitHeight = 70;
                 limitWidth = 70;
                 removeQuery = true;
                 httpOnly = true;
               }
 
-              if (window.location.href.indexOf("staples.com") > -1) {
-                imageTags = document.querySelectorAll(
-                  "#image_gallery_container img"
-                );
+              if (window.location.href.indexOf('staples.com') > -1) {
+                imageTags = document.querySelectorAll("#image_gallery_container img");
                 limitHeight = 70;
                 limitWidth = 70;
                 removeQuery = true;
               }
-
-              if (window.location.href.indexOf("bedbathandbeyond.com") > -1) {
-                const shadowInside =
-                  document.querySelector("#wmHostPdp").shadowRoot;
-                imageTags = shadowInside.querySelectorAll("img");
+              
+              if (window.location.href.indexOf('bedbathandbeyond.com') > -1) {
+                const shadowInside = document.querySelector("#wmHostPdp").shadowRoot;
+                imageTags = shadowInside.querySelectorAll('img');
               }
 
-              const divs = document.querySelectorAll("div[style]");
+              const divs = document.querySelectorAll('div[style]');
 
               let result = [];
               let mainImage = null;
-
+              
               if (divs && divs.length) {
                 for (let i = 0; i < divs.length; i++) {
                   if (divs[i].style.backgroundImage) {
                     const imageUrl = divs[i].style.backgroundImage;
                     const url = imageUrl.slice(4, -1).replace(/"/g, "");
                     const divBox = divs[i].getBoundingClientRect();
-                    if (url & (url.indexOf("http") > -1)) {
-                      if (
-                        divBox.height > 300 &&
-                        divBox.width > 300 &&
-                        divs[i].style.display != "none" &&
-                        divBox.y < 2500
-                      ) {
+                    if (url & url.indexOf('http') > -1) {
+                      if (divBox.height > 300 && divBox.width > 300 && divs[i].style.display != 'none' && divBox.y < 2500) {
                         result.push(url);
-
-                        if (
-                          divBox.height > 400 &&
-                          divBox.width > 400 &&
-                          !mainImage &&
-                          divBox.y < 600
-                        ) {
+                  
+                        if (divBox.height > 400 && divBox.width > 400 && !mainImage && divBox.y < 600) {
                           mainImage = url;
                         }
                       }
@@ -150,23 +126,12 @@ app.get("/scrape", function (req, res) {
               for (let i = 0; i < imageTags.length; i++) {
                 const imageElement = imageTags[i];
                 const bBox = imageElement.getBoundingClientRect();
-                if (
-                  !useSrcset &&
-                  imageElement.naturalHeight >= limitHeight &&
-                  imageElement.naturalWidth >= limitWidth &&
-                  imageElement.style.display != "none" &&
-                  bBox.y < 2000 &&
-                  imageElement.src.indexOf("/flags/") === -1 &&
-                  imageElement.src
-                ) {
+                if (!useSrcset && imageElement.naturalHeight >= limitHeight && imageElement.naturalWidth >= limitWidth && imageElement.style.display != 'none' && bBox.y < 2000 && imageElement.src.indexOf('/flags/') === -1 && imageElement.src) {
                   if (httpOnly) {
-                    if (
-                      imageElement.src.indexOf("http") > -1 &&
-                      imageElement.src.indexOf("http") != 0
-                    ) {
+                    if (imageElement.src.indexOf('http') > -1 && imageElement.src.indexOf('http') != 0) {
                       continue;
                     } else {
-                      if (imageElement.src.indexOf("http") < 0) {
+                      if (imageElement.src.indexOf('http') < 0) {
                         continue;
                       }
                     }
@@ -177,13 +142,7 @@ app.get("/scrape", function (req, res) {
                     result.push(imageElement.src);
                   }
 
-                  if (
-                    imageElement.naturalHeight > 400 &&
-                    bBox.y < 600 &&
-                    bBox.y > 80 &&
-                    !mainImage &&
-                    imageElement.src.indexOf("null") < 0
-                  ) {
+                  if (imageElement.naturalHeight > 400 && bBox.y < 600 && bBox.y > 80 && !mainImage && imageElement.src.indexOf('null') < 0) {
                     if (removeQuery) {
                       mainImage = imageElement.src.split("?")[0];
                     } else {
@@ -192,25 +151,23 @@ app.get("/scrape", function (req, res) {
                   }
                 }
               }
-
+              
               if (!result.length || useSrcset) {
                 for (let i = 0; i < imageTags.length; i++) {
                   const imageElement = imageTags[i];
                   if (imageElement.srcset) {
-                    if (
-                      window.location.href.indexOf("shop.lululemon.com") > -1
-                    ) {
+                    if (window.location.href.indexOf('shop.lululemon.com') > -1) {
                       result = [...result, imageElement.srcset.split(",\n")[0]];
                     } else {
                       result = [...result, imageElement.srcset.split(", ")[0]];
                     }
-                    result = result.map((item) => {
+                    result = result.map(item => {
                       if (removeQuery) {
                         return item.trim().split("?")[0];
                       } else {
                         return item.trim();
                       }
-                    });
+                    })
                     mainImage = result[0];
                   }
                 }
@@ -219,6 +176,7 @@ app.get("/scrape", function (req, res) {
               if (result.length && !mainImage) {
                 mainImage = result[0];
               }
+              
               return { images: result, mainImage };
             });
             product.images = images;
@@ -229,6 +187,9 @@ app.get("/scrape", function (req, res) {
               if (window.location.href.indexOf('bedbathandbeyond.com') > -1) {
                 elements = [...document.querySelector("#wmHostPdp").shadowRoot.querySelectorAll('*')];
               }
+              if (window.location.href.indexOf('homedepot.com') > -1) {
+                elements = [...document.querySelector("div[name='zone-a']").querySelectorAll('*')];
+              }
               if (window.location.href.indexOf('rh.com') > -1) {
                 defaultFontSize = 11;
               }
@@ -238,91 +199,91 @@ app.get("/scrape", function (req, res) {
                 var record = {};
                 const bBox = element.getBoundingClientRect();
                 if (text.length <= 30 && !(bBox.x == 0 && bBox.y == 0)) {
-                record["fontSize"] = parseInt(getComputedStyle(element)["fontSize"]);
+                  record["fontSize"] = parseInt(getComputedStyle(element)["fontSize"]);
                 }
                 record["y"] = bBox.y;
                 record["x"] = bBox.x;
                 record["text"] = text;
                 if(text.indexOf('Sale Price:') > -1 && text.length > 11) {
-                record["text"] = text.replace('Sale Price:', '');
+                  record["text"] = text.replace('Sale Price:', '');
                 }
                 if(text.indexOf('Sale :') > -1) {
-                record["text"] = text.replace('Sale :', '');
+                  record["text"] = text.replace('Sale :', '');
                 }
                 if(text.indexOf('Standard Price:') > -1) {
-                record["text"] = text.replace('Standard Price:', '');
+                  record["text"] = text.replace('Standard Price:', '');
                 }
                 if(text.indexOf('Price') > -1) {
-                record["text"] = text.replace('Price', '');
+                  record["text"] = text.replace('Price', '');
                 }
                 if(text.indexOf('Limited Time Offer') > -1) {
-                record["text"] = text.replace('Limited Time Offer', '');
+                  record["text"] = text.replace('Limited Time Offer', '');
                 }
                 if(text.indexOf('USD') > -1) {
-                record["text"] = text.replace('USD', '');
+                  record["text"] = text.replace('USD', '');
                 }
                 if(text.indexOf('CAD') > -1) {
-                record["text"] = text.replace('CAD', '');
+                  record["text"] = text.replace('CAD', '');
                 }
                 if(text.indexOf('Now') > -1) {
-                record["text"] = text.replace('Now ', '');
+                  record["text"] = text.replace('Now ', '');
                 }
                 if(text.indexOf(',') > -1) {
-                const textArys = text.split(',');
-                if ((parseInt(textArys[textArys.length - 1]) + "").length == 2) {
-                  record["text"] = text.replace(/,([^,]*)$/, ".$1");
-                }
+                  const textArys = text.split(',');
+                  if ((parseInt(textArys[textArys.length - 1]) + "").length == 2) {
+                    record["text"] = text.replace(/,([^,]*)$/, ".$1");
+                  }
                 }
                 if(text.includes('Sale \n\n') && text.length > 10) {
-                record["text"] = text.replace('Sale \n\n', '');
+                  record["text"] = text.replace('Sale \n\n', '');
                 }
                 if(text.indexOf('off - ') > -1) {
-                record["text"] = text.split('off - ')[1];
+                  record["text"] = text.split('off - ')[1];
                 }
                 if (elementStyle.textDecorationLine != 'none') {
-                record['textDecoration'] = true;
+                  record['textDecoration'] = true;
                 } else {
-                record['textDecoration'] = false;
+                  record['textDecoration'] = false;
                 }
                 return record;
               }
               let records = elements.map(createRecordFromElement);
               function canBePrice(record) {
                 if(record["text"].indexOf('Sale :') > -1 && record["text"].length > 6) {
-                record["text"] = record["text"].replace('Sale :', '');
+                  record["text"] = record["text"].replace('Sale :', '');
                 }
                 if(record["text"].indexOf(' Standard Price') > -1 && record["text"].length > 15) {
-                record["text"] = record["text"].replace(' Standard Price', '');
+                  record["text"] = record["text"].replace(' Standard Price', '');
                 }
                 if(record["text"].indexOf('Standard ') > -1 && record["text"].length > 9) {
-                record["text"] = record["text"].replace('Standard ', '');
+                  record["text"] = record["text"].replace('Standard ', '');
                 }
                 if(record["text"].indexOf('Chewy') > -1 && record["text"].length > 5) {
-                record["text"] = record["text"].replace('Chewy', '');
+                  record["text"] = record["text"].replace('Chewy', '');
                 }
                 if(record["text"].indexOf('current price: ') > -1 && record["text"].length > 15) {
-                record["text"] = record["text"].replace('current price: ', '');
+                  record["text"] = record["text"].replace('current price: ', '');
                 }
                 if(record["text"].indexOf(' USD') > -1 && record["text"].length > 4) {
-                record["text"] = record["text"].replace(' USD', '');
+                  record["text"] = record["text"].replace(' USD', '');
                 }
                 if(record["text"].indexOf('Sale \n\n') > -1) {
-                record["text"] = record["text"].replace('Sale \n\n', '');
+                  record["text"] = record["text"].replace('Sale \n\n', '');
                 }
                 record["text"] = record['text'].trim();
-              
+
                 if (
-                record["y"] > 1300 ||
-                record["fontSize"] == undefined ||
-                !record["text"].match(
-                  /(^(US ){0,1}(rs\.|Rs\.|RS\.|\$|€|£|₹|INR|RP|Rp|USD|US\$|CAD|C\$){0,1}(\s){0,1}[\d,]+(\.\d+){0,1}(\s){0,1}(AED){0,1}(€){0,1}(£){0,1}(Rp){0,1}$)/
-                ) ||
-                record["textDecoration"]
+                  record["y"] > 1300 ||
+                  record["fontSize"] == undefined ||
+                  !record["text"].match(
+                    /(^(US ){0,1}(rs\.|Rs\.|RS\.|\$|€|£|₹|INR|RP|Rp|USD|US\$|CAD|C\$){0,1}(\s){0,1}[\d,]+(\.\d+){0,1}(\s){0,1}(AED){0,1}(€){0,1}(£){0,1}(Rp){0,1}$)/
+                  ) ||
+                  record["textDecoration"]
                 ) {
-                return false;
+                  return false;
                 } else {
-                let scRe = /[\$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6Rp]/;
-                if (record["y"] > 90 && record['fontSize'] >= defaultFontSize && (scRe.test(record['text']))) return true;
+                  let scRe = /[\$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6Rp]/;
+                  if (record["y"] > 90 && record['fontSize'] >= defaultFontSize && (scRe.test(record['text']))) return true;
                 }
               }
               let possiblePriceRecords = records.filter(canBePrice);
